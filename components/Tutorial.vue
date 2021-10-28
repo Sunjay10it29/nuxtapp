1c900f58e45050329ca40185a7f70a4c14eacba0
sunjay10it29 <sunnythai786@gmail.com>
2021-10-28 10:11:58 +0700

    docker spec changes

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -1,6 +1,6 @@
 <!-- Please remove this file from your project -->
 <template>
   <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
-   <h1>Hello World 10</h1>
+   <h1>Hello World 11</h1>
   </div>
 </template>

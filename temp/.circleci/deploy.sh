03e26ee6806ed9aab4bfbabadc79be23f68ac81f
sunjay10it29 <sunnythai786@gmail.com>
2021-10-29 17:07:52 +0700

    deployment

---
+++ temp/.circleci/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+ssh-keyscan -H -p 3456 49.0.71.34 >> ~/.ssh/known_hosts
+cat ~/.ssh/id_ed25519 | ssh -v -p 3456 sunjay@49.0.71.34 'cat >> .ssh/authorized_keys'
+ssh -vvv  -o StrictHostKeyChecking=no -p 3456 sunjay@49.0.71.34 "cd /home/sunjay/office-repo-vm/mo-adminui/; git pull origin fx-sunjay; yarn install; yarn build; pm2 reload admin" 

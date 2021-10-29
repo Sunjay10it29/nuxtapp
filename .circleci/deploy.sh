91c5abaa15fcd25f6e6a1a41da92bd6e326c7c32
sunjay10it29 <sunnythai786@gmail.com>
2021-10-29 11:50:29 +0700

    server host

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -10,4 +10,4 @@ git config --global user.name "sunjay"
 # git push --force production master
 # ssh-keyscan -H -p 3456 49.0.71.34 >> ~/.ssh/known_hosts
 # cat ~/.ssh/id_ed25519 | ssh -vvv -p 3456 sunjay@49.0.71.34 'cat >> .ssh/authorized_keys'
-ssh -vvv -o StrictHostKeyChecking=no -p 3456 sunjay@49.0.71.34 "cd /home/sunjay/testproduction/nuxtapp/; pwd; yarn install; git pull origin master; exit" 
+ssh -vvv -oHostKeyAlgorithms=+ssh-dss -o StrictHostKeyChecking=no -p 3456 sunjay@49.0.71.34 "cd /home/sunjay/testproduction/nuxtapp/; pwd; yarn install; git pull origin master; exit" 

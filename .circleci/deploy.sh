559458ea15af958d466254294c748796e52588df
sunjay10it29 <sunnythai786@gmail.com>
2021-10-29 11:05:30 +0700

    server host

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -8,6 +8,6 @@ git config --global user.name "sunjay"
 # git commit -m "Deploying"
 # git remote add production ssh://sunjay@49.0.71.34:3456/~/testproduction/nuxtapp
 # git push --force production master
-ssh-keyscan -vvv -H -p 3456 49.0.71.34 >> ~/.ssh/known_hosts
+ssh-keyscan -H -p 3456 49.0.71.34 >> ~/.ssh/known_hosts
 cat ~/.ssh/id_ed25519 | ssh -vvv -p 3456 sunjay@49.0.71.34 'cat >> .ssh/authorized_keys'
 ssh -vvv -p 3456 sunjay@49.0.71.34 "cd /home/sunjay/testproduction/nuxtapp/; pwd; yarn install; git pull origin master; exit" 

a0127d665ffa2bc4e48f59a602e4cbb7e0987e88
sunjay10it29 <sunnythai786@gmail.com>
2021-10-29 14:20:26 +0700

    server host

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -7,7 +7,7 @@ git config --global user.name "sunjay"
 git init .
 git add .
 git commit -m "Deploying"
-git remote add production ssh://sunjay@49.0.71.34:3456/home/sunjay/testproduction 
+git remote add production ssh://sunjay@49.0.71.34:3456/home/sunjay/testproduction/nuxtapp 
 git push --force production master
 ssh-keyscan -H -p 3456 49.0.71.34 >> ~/.ssh/known_hosts
 cat ~/.ssh/id_ed25519 | ssh -v -p 3456 sunjay@49.0.71.34 'cat >> .ssh/authorized_keys'

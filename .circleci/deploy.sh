d1b8c0373e3a16c18f22077d45385cbec3936aec
sunjay10it29 <sunnythai786@gmail.com>
2021-10-29 14:43:46 +0700

    changes on live server

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -11,4 +11,4 @@ set -e
 # git push --force production master
 ssh-keyscan -H -p 3456 49.0.71.34 >> ~/.ssh/known_hosts
 cat ~/.ssh/id_ed25519 | ssh -v -p 3456 sunjay@49.0.71.34 'cat >> .ssh/authorized_keys'
-ssh -vvv  -o StrictHostKeyChecking=no -p 3456 sunjay@49.0.71.34 "cd /home/sunjay/testproduction/nuxtapp/; pwd; yarn install; yarn build; git pull origin master; pm2 reload test" 
+ssh -vvv  -o StrictHostKeyChecking=no -p 3456 sunjay@49.0.71.34 "cd /home/sunjay/testproduction/nuxtapp/; git pull origin master; yarn install; yarn build; pm2 reload test" 

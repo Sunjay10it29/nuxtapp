50f1e829cd25af0574455edcb05ed6429d169c23
sunjay10it29 <sunnythai786@gmail.com>
2021-10-29 09:37:58 +0700

    server host

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -2,11 +2,10 @@
 
 set -e
 
-# rm -rf .git
-# rm -rf .gitignore
-# git config --global user.email "sunnythai786@gmail.com"
-# git config --global user.name "sunjay"
-# mv .gitignore
+rm -rf .git
+rm -rf .gitignore
+git config --global user.email "sunnythai786@gmail.com"
+git config --global user.name "sunjay"
 git add .
 git commit -m "Deploying"
 git remote add production ssh://sunjay@49.0.71.34:3456/~/testproduction/nuxtapp

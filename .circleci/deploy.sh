3d8d94fa153b1845a0b92bc1e1bf25d6a2ff815c
sunjay10it29 <sunnythai786@gmail.com>
2021-10-29 09:13:07 +0700

    server host

---
+++ .circleci/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# rm -rf .git
+# rm -rf .gitignore
+# git config --global user.email "sunnythai786@gmail.com"
+# git config --global user.name "sunjay"
+# mv .gitignore
+git add .
+git commit -m "Deploying"
+git remote add production ssh://sunjay@49.0.71.34:3456/~/testproduction/nuxtapp
+git push --force production master
+
+ssh -p 3456 sunjay@49.0.71.34 "cd ~/testproduction/nuxtapp/ && yarn install && pm2 status && exit" 

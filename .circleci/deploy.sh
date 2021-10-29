aa7ecba6fb0eedf4776dd053febf778aa0cc8a96
sunjay10it29 <sunnythai786@gmail.com>
2021-10-29 10:35:38 +0700

    server host

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -9,4 +9,4 @@ git config --global user.name "sunjay"
 # git remote add production ssh://sunjay@49.0.71.34:3456/~/testproduction/nuxtapp
 # git push --force production master
 
-ssh -vvv -p 3456 sunjay@192.168.1.110 "cd ~/testproduction/nuxtapp/ && yarn install && pm2 status && exit" 
+ssh -vvv -p 3456 sunjay@192.168.1.110 "cd /home/sunjay/testproduction/nuxtapp/; pwd; yarn install; git pull origin master; exit" 
